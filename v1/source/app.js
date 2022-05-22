0dc9b083dfed8671f2d4df7d9950828268c5293a
MCanerD <mahircanerdogan@hotmail.com>
2022-05-22 08:50:34 +0300

    cors

--- v1/source/app.js
+++ v1/source/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const helmet = require("helmet");
 const config = require("./config");
 const loaders = require("./loaders");
+const cors = require("cors");
 
 config();
 loaders();
@@ -12,4 +13,8 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 
+app.use(
+    cors({ origin: ["http://localhost:8080"], methods: ["GET"], credentials: true })
+  );
+  
 module.exports = app;
