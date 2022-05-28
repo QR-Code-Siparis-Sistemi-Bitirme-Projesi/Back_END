e8bd888b4cf625243b821017cae428187d08fa4c
MCanerD <mahircanerdogan@hotmail.com>
2022-05-28 16:57:09 +0300

    İsimlendirme

--- v1/source/scripts/logger/KullaniciLogger.js
+++ v1/source/scripts/logger/KullaniciLogger.js
@@ -3,7 +3,7 @@ const winston = require("winston");
 const logger = winston.createLogger({
 
   format: winston.format.json(),
-  defaultMeta: { service: "bilgiler-service" }, 
+  defaultMeta: { service: "kullanici-service" }, 
   transports: [
     new winston.transports.File({
       level: "error",
