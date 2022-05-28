18954f6f3b93b9cb005ada6715d67a090509c35d
MCanerD <mahircanerdogan@hotmail.com>
2022-05-28 17:31:49 +0300

    Validations düzenleme.

---
+++ v1/source/validations/SiparisValidate.js
@@ -0,0 +1,55 @@
+
+const Joi = require("joi");
+
+const siparisValidate = joi.Object({
+  
+   masaNo:[{
+     Kahvalti:[{
+     Urun:{type: String},
+     Fiyat:{type: Object}, //objectler double değeri ile değiştirilecek. 
+     İçindekiler:[{
+       
+    }],
+      }],
+      icecekler:[{
+      Urun:{type: String},
+      Fiyat:{type: Object},
+       }],
+    
+        Tek_Bagel:[{
+        Urun:{type: String},
+        Fiyat:{type: Object},
+        }],
+
+         Tatlilar:[{
+          Urun:{type: String},
+          Fiyat:{type: Object},
+           }],
+
+           Sandviçler:[{
+            Urun:{type: String},
+            Fiyat:{type: Object},
+            İçindekiler:[{
+              
+           }],
+             }],
+             
+             Hafta_Sonu_Ozel:[{
+              Urun:{type: String},
+              Fiyat:{type: Object},
+               }],
+
+               Ekstralar:[{
+                Urun:{type: String},
+                Fiyat:{type: Object},
+                 }],
+      
+           }],
+
+});
+
+
+
+module.exports = {
+  urunValidate
+}
