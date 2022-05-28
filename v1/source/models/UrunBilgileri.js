920504bd82e6cef05e175fdc9011ead89037a928
MCanerD <mahircanerdogan@hotmail.com>
2022-05-28 17:48:54 +0300

    Ürün bilgilerinden update ve post kaldırıldı.

--- v1/source/models/UrunBilgileri.js
+++ v1/source/models/UrunBilgileri.js
@@ -99,63 +99,6 @@ const TatilarSchema = new mongoose.Schema(
   );
   
 
-
-KahvaltiSchema.post("save", (doc) => {
-  logger.info("Ürün başarıyla eklendi, eklenen ürün: ", doc);
-});
-
-KahvaltiSchema.post("findOneAndUpdate", (doc) => {
-  logger.info("Başarıyla güncellendi, güncellenen: ", doc);
-});
-
-IceceklerSchema.post("save", (doc) => {
-  logger.info("Ürün başarıyla eklendi, eklenen ürün: ", doc);
-});
-
-IceceklerSchema.post("findOneAndUpdate", (doc) => {
-  logger.info("Başarıyla güncellendi, güncellenen: ", doc);
-});
-
-TekBagelSchema.post("save", (doc) => {
-  logger.info("Ürün başarıyla eklendi, eklenen ürün: ", doc);
-});
-
-TekBagelSchema.post("findOneAndUpdate", (doc) => {
-  logger.info("Başarıyla güncellendi, güncellenen: ", doc);
-});
-
-TatilarSchema.post("save", (doc) => {
-  logger.info("Ürün başarıyla eklendi, eklenen ürün: ", doc);
-});
-
-TatilarSchema.post("findOneAndUpdate", (doc) => {
-  logger.info("Başarıyla güncellendi, güncellenen: ", doc);
-});
-
-SandviclerSchema.post("save", (doc) => {
-  logger.info("Ürün başarıyla eklendi, eklenen ürün:: ", doc);
-});
-
-SandviclerSchema.post("findOneAndUpdate", (doc) => {
-  logger.info("Başarıyla güncellendi, güncellenen: ", doc);
-});
-
-HaftaSonuOzelSchema.post("save", (doc) => {
-  logger.info("Ürün başarıyla eklendi, eklenen ürün: ", doc);
-});
-
-HaftaSonuOzelSchema.post("findOneAndUpdate", (doc) => {
-  logger.info("Başarıyla güncellendi, güncellenen: ", doc);
-});
-
-EkstralarSchema.post("save", (doc) => {
-  logger.info("Ürün başarıyla eklendi, eklenen ürün: ", doc);
-});
-
-EkstralarSchema.post("findOneAndUpdate", (doc) => {
-  logger.info("Başarıyla güncellendi, güncellenen: ", doc);
-});
-
 const kahvalti = mongoose.model("KahvaltiSchema",KahvaltiSchema);
 const icecekler = mongoose.model("IceceklerSchema",IceceklerSchema);
 const TekBagel = mongoose.model("TekBagelSchema",TekBagelSchema);
