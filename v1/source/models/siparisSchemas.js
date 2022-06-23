6e1ecd3fef853305e16fb746e2d2aa206167ece7
Umutcan-YEL <umutcanyel132@gmail.com>
2022-06-23 08:07:58 +0300

    XXX

--- v1/source/models/siparisSchemas.js
+++ v1/source/models/siparisSchemas.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 const SiparisSchema = new mongoose.Schema(
   {
     masaNo: { type: Number },
+    Miktar: { type: Number },
     Urun: { type: Array },
     Ekstralar: { type: Array },
     Not: { type: String },
     ToplamFiyat: { type: Number },
-    SiparisAktifMi:{type:Boolean}
+    SiparisAktifMi: { type: Boolean }
   },
   //TOPLAM ÜRÜN SAYISINI VE TOPLAM FİYATI VERECEK BİR BİÇİMDE DÜZENLENECEK!
   {
